cfb4a3a3e3c3eb3720b435b7aa666c8a2be3de52
Shih Min-Han <73226430+oscar-shih@users.noreply.github.com>
2022-05-23 01:50:37 +0800

    Add files via upload

---
+++ Final Project/Assembly/hw1.s
@@ -0,0 +1,67 @@
+.data
+    n: .word 11
+.text
+.globl __start
+
+FUNCTION:
+    # Todo: Define your own function in HW1
+    addi sp, sp, -8
+    sw x1, 4(sp)
+    sw a0, 0(sp)
+    
+    # if n >= 10
+    addi x5, x0, 10
+    bge a0, x5, Case1
+    
+    # else if 1 <= n < 10
+    addi x5, x0, 1
+    bge a0, x5, Case2
+    
+    # else if n = 0
+    addi t0, x0, 7
+    addi x10, t0, 0
+    addi sp, sp, 8
+    jalr x0, 0(x1)
+    
+Case1:
+    addi x6, x0, 3
+    mul a0, x6, a0  
+    srli a0, a0, 2 # n = 3/4n
+    jal x1, Main
+    addi x6, t0, 0
+    
+    lw x1, 4(sp)
+    lw a0, 0(sp)
+    addi x7, x0 ,2
+    mul t0, x6, x7 # 2*T(3/4n)
+    
+    addi x7, x0, 7
+    mul a0, a0, x7
+    srli a0, a0, 3 # 7/8n
+    add t0, t0, a0 # +7/8n
+    
+    addi t0, t0, -137 # -137
+    addi sp, sp, 8
+    jalr x0, 0(x1)
+    
+Case2:
+    addi a0, a0, -1 # n = n - 1
+    jal x1, Main
+    addi x7, t0, 0
+    
+    lw x1, 4(sp)
+    addi sp, sp, 8
+    
+    addi x6, x0, 2
+    mul t0, x7, x6 # 2*T(n-1)
+    jalr x0, 0(x1)
+
+# Do NOT modify this part!!!
+__start:
+    la   t0, n
+    lw   x10, 0(t0)
+    jal  x1,FUNCTION
+    la   t0, n
+    sw   x10, 4(t0)
+    addi a0,x0,10
+    ecall
---
+++ Final Project/Verilog/CHIP.v
@@ -0,0 +1,314 @@
+// Your code
+module CHIP(clk,
+            rst_n,
+            // For mem_D
+            mem_wen_D,
+            mem_addr_D,
+            mem_wdata_D,
+            mem_rdata_D,
+            // For mem_I
+            mem_addr_I,
+            mem_rdata_I);
+
+    input         clk, rst_n ;
+    // For mem_D
+    output        mem_wen_D  ;
+    output [31:0] mem_addr_D ;
+    output [31:0] mem_wdata_D;
+    input  [31:0] mem_rdata_D;
+    // For mem_I
+    output [31:0] mem_addr_I ;
+    input  [31:0] mem_rdata_I;
+    
+    //---------------------------------------//
+    // Do not modify this part!!!            //
+    // Exception: You may change wire to reg //
+    reg    [31:0] PC          ;              //
+    wire   [31:0] PC_nxt      ;              //
+    wire          regWrite    ;              //
+    wire   [ 4:0] rs1, rs2, rd;              //
+    wire   [31:0] rs1_data    ;              //
+    wire   [31:0] rs2_data    ;              //
+    wire   [31:0] rd_data     ;              //
+    //---------------------------------------//
+
+    // Todo: other wire/reg
+    reg [31:0] read;
+    wire [31:0] imm;
+    wire [31:0] addr;
+
+    //---------------------------------------//
+    // Do not modify this part!!!            //
+    reg_file reg0(                           //
+        .clk(clk),                           //
+        .rst_n(rst_n),                       //
+        .wen(regWrite),                      //
+        .a1(rs1),                            //
+        .a2(rs2),                            //
+        .aw(rd),                             //
+        .d(rd_data),                         //
+        .q1(rs1_data),                       //
+        .q2(rs2_data));                      //
+    //---------------------------------------//
+
+    // Todo: any combinational/sequential circuit
+    // 做完CPU architecture的part後，在這組合成圖上的流程圖
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            PC <= 32'h00010000; // Do not modify this value!!!
+            
+        end
+        else begin
+            PC <= PC_nxt;
+            
+        end
+    end
+endmodule
+
+
+// CPU Architecture
+module imm_generator(inst, imm_o);
+    input [31:0] inst;
+    output [31:0] imm_o;
+    reg [31:0] imm;
+
+    always @(inst) begin
+        // branch instruction: beq, blt, bge, bltu, bgeu
+        if (inst[6:0] == 7'b1100011)begin
+            imm[12] = inst[31];
+            imm[11] = inst[7];
+            imm[10:5] = inst[30:25];
+            imm[4:1] = inst[11:8];
+            imm_o = {{20{imm[12]}}, imm[12:1]}; // make it 32-bit
+        end
+
+        // Load instruction: lb, lh, lw, lbu, lhu 
+        // Logical gates: andi, ori, xori, 
+        // Others: jalr, slti, sltiu
+        else if (inst[6:0] == 7'b0000011 || inst[6:0] == 7'b0010011 || inst[6:0] == 7'b1100111) begin
+            imm[11:0] = inst[31:20];
+            imm_o = {{20{imm[11]}}, imm[11:0]};
+        end
+
+        // Store instruction: sb sh, sw
+        else if (inst[6:0] == 7'b0100011) begin 
+            imm[11:5] = inst[31:25];
+            imm[4:0] = inst[11:7];
+            imm_o = {{20{imm[11]}}, imm[11:0]}; 
+        end
+
+        // Others: lui, auipc
+        else if (inst[6:0] == 7'b0110111 || inst[6:0] == 7'b0010111) begin
+            imm[31:12] = inst[31:12];
+            imm_o = {imm[31:12], {12{1'b0}}};
+        end
+
+        // 寫到這才發現用case可能比較NB.....我好爛==
+        else imm_o = {32{1'b0}};
+    end
+endmodule
+
+module adder(in1, in2, sum);
+    input [31:0] in1;
+    input [31:0] in2;
+    output [31:0] sum;
+    assign sum = in1 + in2;
+endmodule
+
+module andgate(in1, in2, out);
+    input in1, in2;
+    output out;
+    assign out = in1 && in2;
+endmodule
+
+module mux32(in1, in2, sel, out);
+    input [31:0] in1;
+    input [31:0] in2;
+    input sel;
+    output [31:0] out;
+    assign out = (!sel) ? in1 : in2;
+endmodule
+
+module shiftleft(in1, out);
+    input [31:0] in1;
+    output [31:0] out;
+    assign out = {in1[30:0], 1'b0};
+endmodule
+ 
+// 還沒做ALU那部份 QQ
+module ALU(in1, in2, alu_inst, alu_result, zero);
+    input in1, in2, alu_inst;
+    output alu_result, zero;
+
+endmodule
+
+module ALU_control(inst, alu_op, alu_inst, mul_op);
+    input [31:0] inst;
+    input [2:0] alu_op;
+    output [3:0] alu_inst;
+    output mul_op;
+
+endmodule
+
+module Control(inst, branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write);
+    // output 可能不只圖上那些，要支援多一點功能要改這裡成比較大的control
+    input [6:0] inst;
+    output branch, mem_read, mem_to_reg, mem_write, alu_src, reg_write;
+    output [2:0] alu_op;
+
+
+endmodule
+
+module reg_file(clk, rst_n, wen, a1, a2, aw, d, q1, q2);
+
+    parameter BITS = 32;
+    parameter word_depth = 32;
+    parameter addr_width = 5; // 2^addr_width >= word_depth
+
+    input clk, rst_n, wen; // wen: 0:read | 1:write
+    input [BITS-1:0] d;
+    input [addr_width-1:0] a1, a2, aw;
+
+    output [BITS-1:0] q1, q2;
+
+    reg [BITS-1:0] mem [0:word_depth-1];
+    reg [BITS-1:0] mem_nxt [0:word_depth-1];
+
+    integer i;
+
+    assign q1 = mem[a1];
+    assign q2 = mem[a2];
+
+    always @(*) begin
+        for (i=0; i<word_depth; i=i+1)
+            mem_nxt[i] = (wen && (aw == i)) ? d : mem[i];
+    end
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            mem[0] <= 0;
+            for (i=1; i<word_depth; i=i+1) begin
+                case(i)
+                    32'd2: mem[i] <= 32'hbffffff0;
+                    32'd3: mem[i] <= 32'h10008000;
+                    default: mem[i] <= 32'h0;
+                endcase
+            end
+        end
+        else begin
+            mem[0] <= 0;
+            for (i=1; i<word_depth; i=i+1)
+                mem[i] <= mem_nxt[i];
+        end
+    end
+endmodule
+
+
+// 這裡我只做mul，我看投影片似乎只要mul的功能，但它叫mulDiv，不知道要不要支援Div＠＠
+module mulDiv(clk, rst_n, valid, ready, mode, in_A, in_B, out);
+    // Todo: your HW2
+    input         clk, rst_n;
+    input         valid;
+    input  [1:0]  mode; // mode: 0: mulu, 1: divu, 2: and, 3: avg
+    output        ready;
+    input  [31:0] in_A, in_B;
+    output [31:0] out;
+
+    // Definition of states
+    parameter IDLE = 3'd0;
+    parameter MUL  = 3'd1;
+    parameter OUT  = 3'd2;
+
+    // Todo: Wire and reg if needed
+    reg  [ 2:0] state, state_nxt;
+    reg  [ 4:0] counter, counter_nxt;
+    reg  [63:0] shreg, shreg_nxt;
+    reg  [31:0] alu_in, alu_in_nxt;
+    reg  [32:0] alu_out;
+    reg  [31:0] out;   // reg for output "out"
+    reg  [ 0:0] ready; // reg for output "ready"
+
+    always @(*) begin
+        if (state == OUT) begin
+            out = shreg[31:0];
+            ready = 1;
+        end 
+        else begin
+            out = 0;
+            ready = 0;
+        end
+    end
+
+    // Combinational always block
+    // Todo 1: Next-state logic of state machine
+    always @(*) begin
+        case(state)
+            IDLE: begin
+                if (valid) state_nxt = MUL;
+                else state_nxt = IDLE;
+            end
+            MUL: begin
+                if(counter != 5'd31) state_nxt = MUL;
+                else state_nxt = OUT;
+            end
+            default : state_nxt = IDLE;
+        endcase
+    end
+
+    // Todo 2: Counter
+    always @(*) begin
+        if(state == MUL) counter_nxt = counter + 1;
+        else counter_nxt = 0;
+    end
+
+    // ALU input
+    always @(*) begin
+        case(state)
+            IDLE: begin
+                if (valid) alu_in_nxt = in_B;
+                else       alu_in_nxt = 0;
+            end
+            OUT : alu_in_nxt = 0;
+            default: alu_in_nxt = alu_in;
+        endcase
+    end
+
+    // Todo 3: ALU output
+    always @(*) begin
+        if (state == MUL) begin
+            if (shreg[0] == 1'b1) alu_out = alu_in;
+            else alu_out = 0;
+        end
+        else alu_out = 0;
+    end
+
+    // Todo 4: Shift register
+    always @(*) begin
+        case(state)
+            IDLE: begin
+                if (valid) shreg_nxt = {32'b0, in_A};
+                else shreg_nxt = 0;
+            end
+            MUL: begin
+                shreg_nxt = {shreg[63:32] + alu_out[31:0], shreg[31:0]} >> 1;
+                if (shreg_nxt[62:31] < shreg[63:32]) shreg_nxt[63] = 1;
+                else shreg_nxt[63] = 0;
+            end
+            default: shreg_nxt = 0;
+        endcase
+    end
+
+    // Todo: Sequential always block
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            state <= IDLE;
+        end
+        else begin
+            state <= state_nxt;
+            counter <= counter_nxt;
+            shreg <= shreg_nxt;
+            alu_in <= alu_in_nxt;
+        end
+    end
+
+endmodule
